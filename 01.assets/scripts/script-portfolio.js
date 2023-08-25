65cacf81e3c02c5a6d082784782a5bfd5d9e60a4
ditusnova <ditusnova1@gmail.com>
2023-08-25 10:00:06 +0200

    Create script-portfolio.js

---
+++ 01.assets/scripts/script-portfolio.js
@@ -0,0 +1,28 @@
+/**
+ * @copyright InovatorWawe 2023
+ * @author Dita <example@gmail.com>
+ */
+
+"use strict";
+
+/* CHIP */
+
+/* TAB */
+
+const /* {NodeList} */ $tabBtn = document.querySelectorAll("[data-tab-btn]");
+let /* {NodeElement} */ [lastActiveTab] = document.querySelectorAll("[data-tab-content]");
+let /* {NodeElement} */ [lastActiveTabBtn] = $tabBtn;
+
+$tabBtn.forEach(item => {
+    item.addEventListener("click", function() {
+        lastActiveTab.classList.remove("active");
+        lastActiveTabBtn.classList.remove("active");
+
+        const /* {NodeElement} */ $tabContent = document.querySelector(`[data-tab-content="${item.dataset.tabBtn}"]`);
+        $tabContent.classList.add("active");
+        this.classList.add("active");
+
+        lastActiveTab = $tabContent;
+        lastActiveTabBtn = this;
+    });
+});
